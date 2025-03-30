19980f8672475392a53705cbb407eabb87f573f0
BeloMaximka <belomaxim@gmail.com>
2025-03-31 01:48:50 +0300

    Nbu rates cache

--- app/src/main/java/itstep/learning/andrioid_213/RateActivity.java
+++ app/src/main/java/itstep/learning/andrioid_213/RateActivity.java
@@ -1,7 +1,9 @@
 package itstep.learning.andrioid_213;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,11 +18,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import itstep.learning.andrioid_213.rate.NbuRate;
@@ -28,9 +35,13 @@ import itstep.learning.andrioid_213.rate.NbuRateChipView;
 
 public class RateActivity extends AppCompatActivity {
     private final String nbuUrl = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+    private final String cacheFilename = "nbu_rates_cache.json";
+    private static List<NbuRate> cachedNbuRates;
     private TextView tvTmp;
     private LinearLayout ratesContainer;
     private List<NbuRate> nbuRates;
+
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,19 +55,72 @@ public class RateActivity extends AppCompatActivity {
         });
         tvTmp = findViewById(R.id.temp);
         ratesContainer = findViewById(R.id.rates_container);
-        new Thread(this::loadRates).start();
+        if(cachedNbuRates == null) {
+            try(FileInputStream fis = openFileInput(cacheFilename)) {
+                Log.d("onCreate","Nbu got cache from file.");
+                String json = readAllText(fis);
+                loadRates(json);
+            }
+            catch(IOException ignored) {
+                Log.d("onCreate","Nbu not cached.");
+                new Thread(this::loadRatesFromUrl).start();
+            }
+        }
+        else {
+            Log.d("onCreate", "Getting nbu from cache.");
+            nbuRates = cachedNbuRates;
+        }
+
+        if(nbuRates != null && isRatesActual()) {
+            showRates();
+        }
+        else {
+            Log.d("onCreate", "Cache is stale.");
+            new Thread(this::loadRatesFromUrl).start();
+        }
+
+        handler.postDelayed(this::periodicAction, 5000);
     }
 
-    private void loadRates() {
+
+    private void periodicAction() {
+        if(nbuRates != null && isRatesActual()) {
+            showRates();
+        }
+        else {
+            Log.d("periodicAction", "Cache is stale.");
+            new Thread(this::loadRatesFromUrl).start();
+        }
+        handler.postDelayed(this::periodicAction, 5000);
+    }
+    private void loadRatesFromUrl() {
         try  {
             String jsonText = fetchUrlText(nbuUrl);
+            try(FileOutputStream fos = openFileOutput(cacheFilename, Context.MODE_PRIVATE)) {
+                fos.write(jsonText.getBytes(StandardCharsets.UTF_8));
+                Log.d("loadRatesFromUrl", "File cache saved");
+            }
+            catch (IOException ex) {
+                Log.d("loadRatesFromUrl", ex.getCause() + ex.getMessage());
+            }
+            loadRates(jsonText);
+            runOnUiThread(this::showRates);
+        }
+        catch (RuntimeException e) {
+            Log.e("loadRates", "loadRates: " + e.getMessage());
+            runOnUiThread(() -> tvTmp.setText(R.string.rate_tv_load_fail));
+        }
+    }
+
+    private void loadRates(String jsonText) {
+        try  {
             JSONArray arr = new JSONArray(jsonText);
             int len = arr.length();
             nbuRates = new ArrayList<>();
             for (int i = 0; i < len; i++) {
                 nbuRates.add(NbuRate.fromJsonObject((arr.getJSONObject(i))));
             }
-            runOnUiThread(this::showRates);
+            cachedNbuRates = nbuRates;
         }
         catch (RuntimeException | JSONException e) {
             Log.e("loadRates", "loadRates: " + e.getMessage());
@@ -91,16 +155,31 @@ public class RateActivity extends AppCompatActivity {
         }
 
         try (InputStream urlStream = url.openStream()) {
-            byte[] buffer = new byte[4096];
-            ByteArrayOutputStream byteBuilder = new ByteArrayOutputStream();
-            int receivedBytes;
-            while ((receivedBytes = urlStream.read(buffer)) > 0) {
-                byteBuilder.write(buffer, 0 ,receivedBytes);
-            }
-            return byteBuilder.toString();
+            return readAllText(urlStream);
         }
         catch (IOException e) {
             throw new RuntimeException();
         }
+    }
+
+    private boolean isRatesActual() {
+        try {
+            return nbuRates.get(0).getExchangeDate().before(
+                    NbuRate.dateFormat.parse(NbuRate.dateFormat.format(new Date()))
+            );
+        } catch (ParseException e) {
+            Log.d("isRatesActual", e.getCause() + e.getMessage());
+        }
+        return false;
+    }
+
+    private String readAllText(InputStream inputStream) throws IOException {
+        byte[] buffer = new byte[4096];
+        ByteArrayOutputStream byteBuilder = new ByteArrayOutputStream();
+        int receivedBytes;
+        while ((receivedBytes = inputStream.read(buffer)) > 0) {
+            byteBuilder.write(buffer, 0 ,receivedBytes);
+        }
+        return byteBuilder.toString();
     }
 }
